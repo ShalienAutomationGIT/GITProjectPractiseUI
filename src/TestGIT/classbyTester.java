1398a6dfc4a0f3c82b7d48893700a5164b70df4b
ShalienAutomationGIT <shalienkaushik06@gmail.com>
2023-07-06 17:56:14 +0530

    Commit by shalien tester
    New Class created

---
+++ src/TestGIT/classbyTester.java
@@ -0,0 +1,21 @@
+package TestGIT;
+
+public class classbyTester {
+
+	
+	
+public static void main(String[] args) {
+		
+		
+		String str = "This is first Class created by Tester";
+		
+		System.out.println(str);
+		
+		
+		
+		
+	}
+	
+	
+	
+}
