d093cb7b6400a7e1e6a9bf3ca12f2249164dfb81
ShalienAutomationGIT <shalienkaushik06@gmail.com>
2023-07-06 17:09:14 +0530

    This is my first commit as a Lead Uploading proj on master

---
+++ src/TestGIT/GITClass.java
@@ -0,0 +1,19 @@
+package TestGIT;
+
+public class GITClass {
+
+	
+	public static void main(String[] args) {
+		
+		
+		String str = "This is first Class created";
+		
+		System.out.println(str);
+		
+		
+		
+		
+	}
+	
+	
+}
